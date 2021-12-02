5be262e2cebbf628eeb6c9dd2c3d581a600b55f8
jimmy-grande <jimmy.grande@outlook.com>
2021-12-02 13:04:00 +0100

    test(day1): complete day 1, part 2

--- src/day1/index.spec.ts
+++ src/day1/index.spec.ts
@@ -2,15 +2,30 @@ import exercise from "./index";
 import input from "./part1.json";
 
 describe("day1: count larger values", () => {
-  it("part 1: should return the total of values that are larger from previous one", () => {
+  /**
+   * @deprecated
+   */
+  //   it("part 1: should return the total of values that are larger from previous one", () => {
+  //     // ARRANGE
+  //     const simpleDataset = [1, 10, 5];
+  //     const day1Part1Result = 1532;
+  //     // ACT
+  //     const result = exercise(simpleDataset);
+  //     const complexResult = exercise(input);
+  //     // ASSERT
+  //     expect(result).toEqual(1);
+  //     expect(complexResult).toEqual(day1Part1Result);
+  //   });
+
+  it("part 2: should return the total of a measurments that are larger from previous one", () => {
     // ARRANGE
-    const simpleDataset = [1, 10, 5];
-    const day1Part1Result = 1532;
+    const simpleDataset = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    const day1Part2Result = 1571;
     // ACT
     const result = exercise(simpleDataset);
     const complexResult = exercise(input);
     // ASSERT
-    expect(result).toEqual(1);
-    expect(complexResult).toEqual(day1Part1Result);
+    expect(result).toEqual(5);
+    expect(complexResult).toEqual(day1Part2Result);
   });
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,23 +1,27 @@
 type Day1Input = number[];
-type CountLargerMeasurmentFromPrevious = (input: Day1Input) => number;
+type CountLargerMeasurementFromPrevious = (input: Day1Input) => number;
 
 type Aggregate = {
   previousDepth: number;
   count: number;
 };
-const countLargerMeasurmentFromPrevious: CountLargerMeasurmentFromPrevious = (input) => {
+
+type GetMeasurement = (values: number[]) => (index: number) => number;
+
+const getMeasurement: GetMeasurement = (values) => (index) => values[index] + values[index + 1] + values[index + 2];
+const countLargerMeasurementFromPrevious: CountLargerMeasurementFromPrevious = (input) => {
   const defaultValue: Aggregate = {
     previousDepth: NaN,
     count: 0,
   };
-  function reduceDepth(acc: Aggregate, depth: number) {
+  function reduceDepth(acc: Aggregate, depth: number, index: number, values: number[]) {
     return {
       previousDepth: depth,
       count: acc.previousDepth < depth ? ++acc.count : acc.count,
     };
   }
-
-  return input.reduce(reduceDepth, defaultValue).count;
+  const measurements = input.reduce<number[]>((acc, _, index) => [...acc, getMeasurement(input)(index)], []);
+  return measurements.reduce(reduceDepth, defaultValue).count;
 };
 
-export default countLargerMeasurmentFromPrevious;
+export default countLargerMeasurementFromPrevious;

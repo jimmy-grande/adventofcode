e7670d0b8f1a5029e167dc75529b4e3f20a0d4a9
jimmy-grande <jimmy.grande@outlook.com>
2021-12-02 13:53:01 +0100

    test(day2): complete day 2, part 2

--- src/day2/index.spec.ts
+++ src/day2/index.spec.ts
@@ -2,7 +2,29 @@ import getPosition, { Day2Input } from "./index";
 import input from "./input.json";
 
 describe("day 2: calculate horizontal postion and depth", () => {
-  it("part 1: should return the sum of horizontal position and depth", () => {
+    /**
+     * @deprecated
+     */
+//   it("part 1: should return the sum of horizontal position and depth", () => {
+//     // ARRANGE
+//     const simpleDataset: Day2Input = [
+//       ["forward", 5],
+//       ["down", 5],
+//       ["forward", 8],
+//       ["up", 3],
+//       ["down", 8],
+//       ["forward", 2],
+//     ];
+//     const day2Part1Result = 2272262;
+//     // ACT
+//     const result = getPosition(simpleDataset);
+//     const complexResult = getPosition(input as Day2Input);
+//     // ASSERT
+//     expect(result).toEqual(150);
+//     expect(complexResult).toEqual(day2Part1Result);
+//   });
+
+  it("part 2: should return the sum of horizontal position and depth based on aim", () => {
     // ARRANGE
     const simpleDataset: Day2Input = [
       ["forward", 5],
@@ -12,12 +34,12 @@ describe("day 2: calculate horizontal postion and depth", () => {
       ["down", 8],
       ["forward", 2],
     ];
-    const day2Part1Result = 2272262;
+    const day2Part2Result = 2134882034;
     // ACT
     const result = getPosition(simpleDataset);
     const complexResult = getPosition(input as Day2Input);
     // ASSERT
-    expect(result).toEqual(150);
-    expect(complexResult).toEqual(day2Part1Result);
+    expect(result).toEqual(900);
+    expect(complexResult).toEqual(day2Part2Result);
   });
 });
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -2,20 +2,24 @@ type Move = "forward" | "up" | "down";
 export type Day2Input = Array<[Move, number]>;
 type GetPosition = (input: Day2Input) => number;
 
+/**
+ * @deprecated
+ */
 type PositionV1 = [horizontalPosition: number, depth: number];
+type PositionV2 = [horizontalPosition: number, depth: number, aim: number];
 const getPosition: GetPosition = (input) => {
-  const defaultPosition: PositionV1 = [0, 0];
+  const defaultPosition: PositionV2 = [0, 0, 0];
 
-  const reducePositions = function (acc: PositionV1, [move, x]: [Move, number]): PositionV1 {
-    const [hPos, depth] = acc;
+  const reducePositions = function (acc: PositionV2, [move, x]: [Move, number]): PositionV2 {
+    const [hPos, depth, aim] = acc;
     if (move === "forward") {
-      return [hPos + x, depth];
+      return [hPos + x, depth + aim * x, aim];
     }
     if (move === "up") {
-      return [hPos, depth - x];
+      return [hPos, depth, aim - x];
     }
     if (move === "down") {
-      return [hPos, depth + x];
+      return [hPos, depth, aim + x];
     }
     return acc;
   };
